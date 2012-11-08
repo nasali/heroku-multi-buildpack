a07f0a760acc7c233276e74f3556ae62ef2eff47
Nass <nasali@anypresence.com>
2012-11-08 12:22:42 -0500

    Changes.

--- lib/multipack.rb
+++ lib/multipack.rb
@@ -1,5 +1,8 @@
-TRIGGER_FILE = ".buildpacks"
+TRIGGER_FILE_NAME = ".buildpacks"
+
 WORK_DIR = ARGV[0]
+
+TRIGGER_FILE = File.join(WORK_DIR,TRIGGER_FILE_NAME)
 
 def log(message)
   puts "      #{message}"
