52eaf0aded4b5b18464b8ff9eb7ee16420e6895a
Nass <nasali@anypresence.com>
2012-11-08 20:13:12 -0500

    Handling single values.

--- lib/multipack.rb
+++ lib/multipack.rb
@@ -23,17 +23,27 @@ def parse_release_vars(vars_string)
     @env_yaml = YAML.load(vars_string)
   else
     yaml = YAML.load(vars_string)
-    @env_yaml.merge!(yaml) do |key, old_value, new_value| 
-      old_value.merge!(new_value) do |sub_key, sub_old_value, sub_new_value| 
-        if sub_old_value.kind_of? Hash
-          sub_old_value.merge!(sub_new_value)
-        elsif sub_old_value.kind_of? Array
-          sub_old_value << sub_new_value 
-        elsif sub_old_value.kind_of? String
-          sub_old_value << ":#{sub_new_value}"
-        else
-          raise "I do not know what to do with #{sub_old_value}"
+    @env_yaml.merge!(yaml) do |key, old_value, new_value|
+      if old_value.kind_of? Hash
+        
+        old_value.merge!(new_value) do |sub_key, sub_old_value, sub_new_value| 
+          if sub_old_value.kind_of? Hash
+            sub_old_value.merge!(sub_new_value)
+          elsif sub_old_value.kind_of? Array
+            sub_old_value << sub_new_value 
+          elsif sub_old_value.kind_of? String
+            sub_old_value << ":#{sub_new_value}"
+          else
+            raise "I do not know what to do with #{sub_old_value}"
+          end
         end
+        
+      elsif old_value.kind_of? Array
+        old_value << new_value 
+      elsif old_value.kind_of? String
+        old_value << ":#{new_value}"
+      else
+        raise "I do not know what to do with #{old_value}"
       end
     end
     puts "We made this:\n#{@env_yaml.inspect}\n0000"
